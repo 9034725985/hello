3c54bd834277cf7ee2e02d4c2e9a8bf6af354ca2
Kushal <kushaldeveloper@gmail.com>
2019-02-10 20:34:27 -0500

    add Spanish support

--- hello.go
+++ hello.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const spanish = "Spanish"
 
 // Hello returns a string
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishPrefix + name
 	}
 	return englishPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
